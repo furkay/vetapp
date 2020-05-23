eebf91815f38fdc9207f672aa7b0a5e501ceb7c0
Furkan Saffet OLKAY <32885005+furkay@users.noreply.github.com>
2020-05-23 20:03:28 +0300

    vet set.

--- lib/features/model/vet.dart
+++ lib/features/model/vet.dart
@@ -1,30 +1,27 @@
 class Vet {
   int vetID;
-  String name;
-  String adress;
-  String number;
-  int level;
+  int vergiNo;
   String klinik;
 
-  Vet({this.vetID, this.name, this.adress, this.number, this.klinik});
+  Vet({this.vetID, this.vergiNo, this.klinik});
 
-  Vet.fromJson(Map<String, dynamic> json) {
-    vetID = json['vetID'];
-    name = json['name'];
-    adress = json['adress'];
-    number = json['number'];
-    level=json['level'];
-    klinik = json['klinik'];
-  }
+  // Vet.fromJson(Map<String, dynamic> json) {
+  //   vetID = json['vetID'];
+  //   name = json['name'];
+  //   adress = json['adress'];
+  //   number = json['number'];
+  //   level=json['level'];
+  //   klinik = json['klinik'];
+  // }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['vetID'] = this.vetID;
-    data['name'] = this.name;
-    data['adress'] = this.adress;
-    data['number'] = this.number;
-    data['level']=this.level;
-    data['klinik'] = this.klinik;
-    return data;
-  }
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = new Map<String, dynamic>();
+  //   data['vetID'] = this.vetID;
+  //   data['name'] = this.name;
+  //   data['adress'] = this.adress;
+  //   data['number'] = this.number;
+  //   data['level']=this.level;
+  //   data['klinik'] = this.klinik;
+  //   return data;
+  // }
 }
