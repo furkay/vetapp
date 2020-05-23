789b7535a9b9384c306e2fc5b78996c59af06a4c
tascienes <49910287+tascienes@users.noreply.github.com>
2020-05-23 19:57:07 +0300

    user register added

--- lib/features/view/userviews/user_add_page.dart
+++ lib/features/view/userviews/user_add_page.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vetapp/core/view/base/base_state.dart';
 import 'package:vetapp/core/view/widget/TextFormField/build_custom_field.dart';
 import 'package:vetapp/core/view/widget/TextFormField/text_form_field_object.dart';
-
 import 'package:vetapp/features/model/user.dart';
-
-import 'dart:math';
-
 import 'package:vetapp/features/service/user_db.dart';
 
 class UserAdd extends StatefulWidget {
@@ -18,7 +14,11 @@ class _UserAddState extends BaseState<UserAdd> {
   TextEditingController userName = TextEditingController();
   TextEditingController adress = TextEditingController();
   TextEditingController number = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   String userLevel = 'Uye';
+  String hata;
+  bool taxVisible = false;
+
   @override
   Widget build(BuildContext context) {
     //TextField Create
@@ -37,13 +37,18 @@ class _UserAddState extends BaseState<UserAdd> {
     List<Widget> children = [];
     fields.forEach((element) {
       children.add(
-          buildTextFormField(field: element, padding: insetHorizontal(0.2)));
+        buildTextFormField(field: element, padding: insetHorizontal(0.2)),
+      );
     });
 
     //end
 
     return Scaffold(
-      appBar: AppBar(),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text("Kayıt"),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
@@ -53,51 +58,78 @@ class _UserAddState extends BaseState<UserAdd> {
                 children: children,
               ),
             ),
-            Row(
-              children: [
-                Flexible(
-                  child: RadioListTile(
-                    groupValue: userLevel,
-                    title: Text('Üye'),
-                    value: 'Uye',
-                    onChanged: (val) {
-                      setState(() {
-                        userLevel = val;
-                      });
-                    },
-                  ),
+            Visibility(
+              visible: taxVisible,
+              child: Container(
+                padding: insetHorizontal(0.2),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.info_outline),
+                      hintText: "Vergi no"),
                 ),
-                Flexible(
-                  child: RadioListTile(
-                    groupValue: userLevel,
-                    title: Text('Veteriner'),
-                    value: 'Veteriner',
-                    onChanged: (val) {
-                      setState(() {
-                        userLevel = val;
-                      });
-                    },
-                  ),
-                ),
-              ],
+              ),
             ),
-            RaisedButton.icon(
-              onPressed: () {
-                int i = Random().nextInt(10000);
-                UserDB().addUser(User(
-                  name: userName.text,
-                  adress: adress.text,
-                  number: number.text,
-                  level: userLevel,
-                ));
-                //.then((value) => Navigator.of(context).pop());
-              },
-              icon: Icon(Icons.send),
-              label: Text("Kaydet"),
+            Padding(
+              padding: insetHorizontal(0.1),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: RadioListTile(
+                      groupValue: userLevel,
+                      title: Text('Üye'),
+                      value: 'Uye',
+                      onChanged: (val) {
+                        taxVisible = false;
+                        setState(() {
+                          userLevel = val;
+                        });
+                      },
+                    ),
+                  ),
+                  Flexible(
+                    child: RadioListTile(
+                      groupValue: userLevel,
+                      title: Text('Veteriner'),
+                      value: 'Veteriner',
+                      onChanged: (val) {
+                        taxVisible = true;
+                        setState(() {
+                          userLevel = val;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Builder(
+              builder: (context) => RaisedButton.icon(
+                onPressed: () {
+                  UserDB()
+                      .addUser(User(
+                        name: userName.text,
+                        adress: adress.text,
+                        number: number.text,
+                        level: userLevel,
+                      ))
+                      .then((value) => value != null
+                          ? showSnackbar("Bu kullanıcı adı zaten kayıtlı")
+                          : null);
+                },
+                icon: Icon(Icons.send),
+                label: Text("Kaydet"),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  showSnackbar(String message) {
+    return _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text('$message'),
+      duration: Duration(seconds: 2),
+    ));
   }
 }
